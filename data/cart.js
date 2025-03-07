58cb5d2919607ecd7be7927b211f11b76cae13c8
SachinWeapon <sachinpanigrahi03@gmail.com>
2025-03-08 00:08:47 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,15 @@
 import { products } from "./products.js";
 
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
 
 export function addtocart(productId)
 {
@@ -29,8 +32,13 @@ export function addtocart(productId)
       quantity:quantity
     });
   }
+  saveToStorage();
 }
 
+function saveToStorage()
+{
+  localStorage.setItem('cart',JSON.stringify(cart));
+} 
 export function removeFromCart(productId)
 {
   const newCart=[]; 
@@ -42,4 +50,5 @@ export function removeFromCart(productId)
   });
 
   cart=newCart;
+  saveToStorage();
 }
